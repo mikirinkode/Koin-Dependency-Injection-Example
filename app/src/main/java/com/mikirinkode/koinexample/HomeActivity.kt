e5ae2079d9761b02d5037ae3020b11121b43e3b5
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-29 10:00:57 +0700

    create simple login form

---
+++ app/src/main/java/com/mikirinkode/koinexample/HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.mikirinkode.koinexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mikirinkode.koinexample.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+    lateinit var userRepository: UserRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sesi = SessionManager(this)
+        userRepository = UserRepository.getInstance(sesi)
+
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
+
+        binding.btnLogout.setOnClickListener {
+            userRepository.logoutUser()
+            moveToMainActivity()
+        }
+    }
+
+    private fun moveToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
